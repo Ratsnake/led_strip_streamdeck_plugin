9367b866f8fd4eda8b347f40735a7b559d722470
stonericefield <89579455+Ratsnake@users.noreply.github.com>
2024-11-07 16:58:22 +0900

    プロジェクト ファイルを追加します。

---
+++ led_strip_plugin.cmd
@@ -0,0 +1,2 @@
+﻿cd %appdata%\Elgato\StreamDeck\Plugins\com.yourcompany.plugin.action
+led_strip_plugin.exe -break %*
---
+++ models/CounterSettingsModel.cs
@@ -0,0 +1,7 @@
+﻿namespace led_strip_plugin.Models
+{
+  public class CounterSettingsModel
+  {
+	public int Counter { get; set; } = 0;
+  }
+}
